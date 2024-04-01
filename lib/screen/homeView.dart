f31ad7ec9e335667dc9b1f52a551320f1a757b97
Muhammad Davin Widyatmaka <davin.new58@gmail.com>
2024-04-01 19:07:26 +0700

    add logika pengambilan dan penampilan data dengan futurebuild

--- lib/screen/homeView.dart
+++ lib/screen/homeView.dart
@@ -1,4 +1,5 @@
 import 'package:data_kontak/controller/kontak_controller.dart';
+import 'package:data_kontak/model/person.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -19,6 +20,24 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Daftar Orang"),
+      ),
+      body: FutureBuilder<List<person>>(
+        future: _controller.getPeople(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else {
+            return ListView.builder(
+                itemCount: snapshot.data?.length ?? 0,
+                itemBuilder: (context, index) {});
+          }
+        },
+      ),
+    );
   }
 }
